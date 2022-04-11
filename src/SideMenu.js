743959439e95977bc52d5086a03d1318b1386381
mofe <mofeomotosho8@gmail.com>
2022-04-10 23:57:07 -0300

    setting up SideMenu ..

---
+++ src/SideMenu.js
@@ -0,0 +1,18 @@
+import './SideMenu.css';
+
+function SideMenu() {
+  return (
+    <div className="side-menu">
+      <div className='Rectangle-239'>
+          <p>
+              SLACA 2019
+          </p>
+      </div>
+      <div className='dewey4'>
+        <img src={require('./images/dewey4.png') }></img>
+      </div>
+    </div>
+  );
+}
+
+export default SideMenu;
