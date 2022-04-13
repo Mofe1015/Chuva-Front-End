31334a0c9ae5e81f2869b4c380dc4637bd84da6f
mofe <mofeomotosho8@gmail.com>
2022-04-13 12:47:22 -0300

    adding (ver mais) to  Resumo body

--- src/Content/Resumo.js
+++ src/Content/Resumo.js
@@ -1,20 +1,26 @@
 import React from 'react';
 import './ContentStyling/Resumo.css';
-import { IoMdDownload } from "react-icons/io";
 
 
 function Resumo(){
     const [expand, setExpand] = React.useState(true)
+
     function expandcollapse(){
+        // expanding resumo 
         if (expand == true){
             document.getElementById('resumoid').style.transition = '0.2s'
             document.getElementById('resumoid').style.maxHeight = '450px'
             document.getElementById('resumoid').style.height = '450px'
-        } else{
+            document.getElementById('vermaisid').style.display = 'none'
+        } 
+        // collapsing resumo
+        else{
             document.getElementById('resumoid').style.transition = '0.2s'
             document.getElementById('resumoid').style.maxHeight = '189px'
             document.getElementById('resumoid').style.height = '189px'
+            document.getElementById('vermaisid').style.display = 'flex'
         }
+        // updating resumo state
         setExpand(() => !expand);
     }
     
@@ -32,7 +38,7 @@ function Resumo(){
                 <p>
                     Fusce vitae luctus dui. Donec id euismod mauris, in volutpat urna. Proin dapibus consequat feugiat. Proin dictum justo arcu, quis vestibulum augue lacinia quis. Sed dignissim dui nulla, ut faucibus mauris sodales id. Aliquam erat volutpat. Maecenas dolor enim, tincidunt id elit non, suscipit interdum turpis. Etiam finibus urna libero, eget interdum eros volutpat ullamcorper. Pellentesque et pretium lorem. Aenean interdum quis diam ac porttitor. Cras nec ipsum pulvinar, pharetra libero non, ornare enim. Etiam in laoreet odio.
                 </p>
-
+                <div id='vermaisid' className='resumo-body-ver-mais'>....<p>ver mais</p></div>
                 <p>
                     Nam eget tristique elit, at fermentum tellus. Mauris scelerisque ligula id eleifend feugiat. Donec eleifend vehicula sem nec dapibus. Integer scelerisque neque dui, in lacinia erat molestie eu. Phasellus maximus dui eget lacus porta tempor. Ut ex nibh, dignissim quis purus semper, efficitur facilisis turpis. Praesent blandit elementum ultricies. Aliquam sit amet enim sit amet nulla pulvinar lobortis consectetur non odio. Phasellus at lacus hendrerit, vulputate nisi sit amet, viverra mauris. Etiam eu scelerisque orci. Quisque sagittis, mi vitae pharetra iaculis, orci elit eleifend massa, eu posuere mauris odio id odio. Morbi eu libero luctus, consectetur lorem vel, interdum sapien. Aenean in porta arcu. Maecenas eu maximus massa.
                 </p>
@@ -40,6 +46,7 @@ function Resumo(){
                 <p>
                     Praesent velit dolor, dignissim sed quam ac, efficitur porta justo. Pellentesque porta pharetra felis ut hendrerit. Nulla facilisi. Aliquam erat volutpat. Nunc sit amet faucibus quam. Maecenas dapibus luctus dolor at viverra. Duis nec fringilla libero. Duis risus nibh, viverra ac orci nec, iaculis dictum sem. Aliquam at malesuada arcu. Aliquam erat volutpat. Donec varius ipsum purus, ut vehicula purus placerat vitae. Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
                 </p>
+                
             </div>
         </div>
     )
