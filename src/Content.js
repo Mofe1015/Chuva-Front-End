6692c3a58955a006a77f04d6c40c459050ce95a3
mofe <mofeomotosho8@gmail.com>
2022-04-13 10:22:37 -0300

    preppig for RESUMO

--- src/Content.js
+++ src/Content.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Styling/Content.css';
 import TopContent from './Content/TopContent';
+import Resumo from './Content/Resumo';
 
 
 function Content(){
     return(
         <div className='content'>
             <TopContent/>
+            <Resumo/>
+            
         </div>
     )
 }
---
+++ src/Content/Resumo.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './ContentStyling/Resumo.css';
+import { IoMdDownload } from "react-icons/io";
+
+
+function Resumo(){
+    return(
+        <div className='resumo'>
+            mofe
+        </div>
+    )
+}
+
+export default Resumo;
